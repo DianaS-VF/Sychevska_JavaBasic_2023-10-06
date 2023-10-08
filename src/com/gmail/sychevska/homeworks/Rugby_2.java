04240e25a0883a623eb9a5da1e44b2a73376b965
disychevska <sychevska@gmail.com>
2023-10-08 19:14:09 +0300

    Display array data in column

---
+++ src/com/gmail/sychevska/homeworks/Rugby_2.java
@@ -0,0 +1,31 @@
+package com.gmail.sychevska.homeworks;
+
+import java.util.Random;
+
+public class Rugby_2 {
+    public static void main(String[] args) {
+
+        int numberOfPlayersInTeam = 25;
+        int[] array = new int[numberOfPlayersInTeam];
+        int minValue = 18;
+        int maxValue = 40;
+        String firstTeamName = "Bulls";
+        String secondTeamName = "Lions";
+
+        Random random = new Random();
+
+
+        System.out.println("Age of players of \"" + firstTeamName + "\": ");
+        for (int i = 0; i < numberOfPlayersInTeam; i++) {
+            System.out.println("Player" + " " + (i + 1) + " - " + random.nextInt(minValue, maxValue + 1) + " years");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Age of players of \"" + secondTeamName + "\": ");
+        for (int i = 0; i < numberOfPlayersInTeam; i++) {
+            System.out.println("Player" + " " + (i + 1) + " - " + random.nextInt(minValue, maxValue + 1) + " years");
+        }
+    }
+}
